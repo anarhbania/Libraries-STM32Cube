4779019f83f7b2c7f62255e8d77bab94c8f20e66
Marcin Bania <54779555+anarhbania@users.noreply.github.com>
2025-02-13 20:24:24 +0100

    Add files via upload

---
+++ NTC10k/Inc/NTC10k.h
@@ -0,0 +1,15 @@
+/*
+ * NTC.h
+ *
+ *  Created on: Aug 25, 2024
+ *      Author: Bania
+ */
+
+#ifndef NTC10K_H_
+#define NTC10K_H_
+
+#include "main.h"
+
+float NTC_Convert(uint16_t value);
+
+#endif

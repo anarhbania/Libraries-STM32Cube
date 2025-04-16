39d876848160b1b52b25adb4057bade5556caa88
Marcin Bania <54779555+anarhbania@users.noreply.github.com>
2025-04-16 21:45:25 +0200

    Add files via upload

--- Language-CPP/Conversion/Inc/conversion.h
+++ Language-CPP/Conversion/Inc/conversion.h
@@ -18,11 +18,16 @@ class ConversionClass
 
 	void shortToByteArray(uint16_t value, uint8_t *array);
 	void floatToByteArray(float value, uint8_t *array);
-	uint16_t byteToShort(uint8_t byte0, uint8_t byte1);
-	uint32_t byteToInt(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3);
-	float byteToFloat(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3);
+
+	uint16_t byteToShort(const uint8_t byte0, const uint8_t byte1);
+	uint32_t byteToInt(const uint8_t byte0, const uint8_t byte1, const uint8_t byte2, const uint8_t byte3);
+	float byteToFloat(const uint8_t byte0, const uint8_t byte1, const uint8_t byte2, const uint8_t byte3);
+
+	int32_t StringToInt(const char *buffer);
+	int32_t StringToLong(const char *buffer);
+	float StringToFloat(const char *buffer);
 };
 
 extern ConversionClass Conversion;
 
-#endif /* CONVERSION_H_ */
+#endif /* FX3U_24MR_TOOL_CONVERSION_H_ */
